3decbfab633537007aa11fa0b022342acf033a77
OnealRj <rloneal19@gocathedral.com>
2023-07-08 19:26:22 -0400

    adding some more changes to respository

--- TikTok0/TikTok0/Delegates/AppDelegate.swift
+++ TikTok0/TikTok0/Delegates/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.rootViewController = TabBarViewController()
         self.window = window
         self.window?.makeKeyAndVisible()
+        FirebaseApp.configure()
         return true
     }
 
